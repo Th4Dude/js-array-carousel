8dc116b75a28e5834dde1744bf54c9dab91c8a27
Th4Dude <gallottimaurizio@yahoo.it>
2023-02-27 18:56:41 +0200

    correzione ed aggiunta commenti

--- js/main.js
+++ js/main.js
@@ -1,25 +1,49 @@
 'use strict'
 
+/* array */
+
 const imgs = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp']
 
-let items_content = '';
-let active = 0
+/* array */
 
 const slider = document.querySelector('.slider')
+
+let active = 0
+
+let items_content = '';
+
+/* ciclo immagini */
 
 for ( let i = 0; i < imgs.length; i++) {
     console.log(imgs[i])
     items_content += `<div class="item"><img src="img/${imgs[i]}" alt=""></div>`
 }
 
+/* ciclo immagini */
+
 slider.innerHTML = slider.innerHTML + items_content;
+
+/* aggiunta class show per mostrare l'immagine */
+
 document.querySelector('.item').classList.add('show')
+
+/* aggiunta class show per mostrare l'immagine */
+
+/* variabili button  */
 
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
+
+/* variabili button  */
+
+/* variabile items per prendere tutte le immagini */
+
 const items = document.querySelectorAll('.item')
 console.log(items)
 
+/* variabile items per prendere tutte le immagini */
+
+/* evento click */
 
 prev.addEventListener('click', function () {
     if (active > 0) {
@@ -36,4 +60,7 @@ next.addEventListener('click', function () {
         active++;
         items[active].classList.add('show');
     }
+    
 });
+
+/* evento click */
